92773061b4ccd1b8d58f6a9af02fe72e144c5252
ericblask <eric.blask@gmail.com>
2026-05-24 19:58:58 -0400

    Update todaygames.js
    added code initialization

--- js/todaygames.js
+++ js/todaygames.js
@@ -1,7 +1,20 @@
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 import { getFormattedDate, extractDateFromField } from "./dateUtils.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 
-const db = getDatabase();
+// 1. YOUR FIREBASE CONFIG
+const firebaseConfig = {
+    apiKey: "AIzaSyCKV4Q_7dJ_fPk6WK4rQs8GiZRvCKhgpng",
+    authDomain: "worldcup2026-5219e.firebaseapp.com",
+    databaseURL: "https://worldcup2026-5219e-default-rtdb.firebaseio.com/",
+    projectId: "worldcup2026-5219e",
+    storageBucket: "worldcup2026-5219e.appspot.com",
+    messagingSenderId: "1089995362453",
+    appId: "1:1089995362453:web:976781ec8aaf63477c0b9c"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 const target = getFormattedDate(new Date());
 
 onValue(ref(db, 'schedules'), (snapshot) => {
